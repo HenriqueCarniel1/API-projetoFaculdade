d0050fbd097e8230f374bc015b709bf6a5efbf52
Gustavo Henrique <gustavocarniel718@gmail.com>
2024-08-28 16:31:50 -0300

    fazendo o login e o registro de usuários

--- routes/routes.js
+++ routes/routes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const Router = express();
 
 // Controllers
+const SendRegisterData = require('../controllers/SendRegisterData');
 const SendLoginData = require('../controllers/SendLoginData');
 
 //Middleware
 
 //Routes
 Router.post('/send/login/user', SendLoginData.SendLoginData);
-Router.post('/send/register/user', SendLoginData.SendLoginData);
+Router.post('/send/register/user', SendRegisterData.SendRegisterData);
 
 
 
